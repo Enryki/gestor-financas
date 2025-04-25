6e0e5dd3e714908dcd92823b3604a7c31f1c6339
Enryki <henriquewatanabe22@gmail.com>
2025-04-25 16:55:29 -0300

    Editando componente para inserção de valores

--- src/app/inserir-valores/inserir-valores.component.ts
+++ src/app/inserir-valores/inserir-valores.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
 import { FormsModule, FormGroup, ReactiveFormsModule, FormBuilder, FormControl } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
-import {FloatLabelType} from '@angular/material/form-field';
+import {FloatLabelType, MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-inserir-valores',
-  imports: [MatCardModule, MatInputModule, MatRadioModule, FormsModule, ReactiveFormsModule, MatIconModule],
+  imports: [MatCardModule, MatInputModule, MatRadioModule, FormsModule, ReactiveFormsModule, MatIconModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './inserir-valores.component.html',
   styleUrl: './inserir-valores.component.scss'
 })
@@ -20,11 +21,17 @@ export class InserirValoresComponent {
     ngOnInit(): void {
       this.inserirForm = this.fb.group({
         tipo_insercao: [''],
-        valor: ['']
+        valor: [''],
+        categoria: [''],
+        observacao: ['']
       });
     }
 
     readonly tipo_insercao = new FormControl('1' as FloatLabelType);
+
+    insere_valor(){
+      console.log('teste');
+    }
 
 
 }
